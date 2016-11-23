1eee4c0a28a229552a5dafa1633feccc54fb6b0e
Mark He <1056977707@qq.com>
2016-11-22 20:19:25 -0500

    add models relationships

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    public function categories()
+    {
+        return $this->belongsTo('App\Category');
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany('App\Tag');
+    }
 }
